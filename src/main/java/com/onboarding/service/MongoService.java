827b08fd57f628f80cbfc9375194e82033784539
ahmedrayf <ahmedrayf@hotmail.com>
2025-04-13 22:25:16 +0200

    Add Invoice sample.

--- src/main/java/com/onboarding/service/MongoService.java
+++ src/main/java/com/onboarding/service/MongoService.java
@@ -9,7 +9,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -20,7 +19,6 @@ public class MongoService {
     private final InvoiceRepository invoiceRepo;
     private final InvoiceMapper invoiceMapper;
 
-    @Transactional
     public void saveAll(List<Invoice> invoices) {
         if (invoices == null || invoices.isEmpty())
             throw new IllegalArgumentException("Invoices is null or empty");
