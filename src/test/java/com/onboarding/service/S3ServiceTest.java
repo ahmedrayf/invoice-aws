c04f09fc7880449d767272316c1ae6042073ffa6
ahmedrayf <ahmedrayf@hotmail.com>
2025-04-14 13:52:17 +0200

    Update S3Service  test.

--- src/test/java/com/onboarding/service/S3ServiceTest.java
+++ src/test/java/com/onboarding/service/S3ServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.util.ResourceUtils;
 import software.amazon.awssdk.core.ResponseInputStream;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
@@ -13,8 +14,10 @@ import software.amazon.awssdk.services.s3.model.GetObjectResponse;
 import software.amazon.awssdk.services.s3.model.S3Exception;
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -27,25 +30,26 @@ class S3ServiceTest {
     private S3Client s3Client;
     @InjectMocks
     private S3Service s3Service;
+    private static final String INVOICE_NAME = "invoice_20250301.csv";
 
     @Test
     void getFileInputStream_ShouldWrapS3Response() throws IOException  {
         // Arrange
-        String testContent = "9999999999|;1234567890|;03.01.2024|;26.11.2023|;25.12.2023|;0800 / 1071020|;73730|;Esslingen|;Teststr. 10 /1|;Firma|;XXX GmbH|;|;|;EUR|;19%|;183.97|;154.60|;29.37|;566655555|;99999999|;03|;|;00281|;false|;0.000000|;11.12.2023|;DE999999999999999999|;XXXXX66XXX|;DE04CCB0009999988A1019999991E002|;\n" +
-                "9999999998|;1234567899|;03.01.2024|;26.11.2023|;25.12.2023|;0800 / 1071020|;74564|;Crailsheim|;Musterplatz 1|;Firma|;Stiftung XYV |;|;|;EUR|;19%|;33.22|;27.92|;5.30|;5566666655|;99999999|;14|;|;00192|;false|;0.000000|;14.12.2023|;DE999999999999999999|;XXXXX66XXX|;DE04CCB0009999999A1019999991E002|;";
+        File file = ResourceUtils.getFile("src/test/resources/invoices/success/csv/invoice_20250301.csv");
+        String content = Files.readString(file.toPath());
 
         ResponseInputStream<GetObjectResponse> mockResponse =
                 new ResponseInputStream<>(
                         GetObjectResponse.builder().build(),
-                        new ByteArrayInputStream(testContent.getBytes()));
+                        new ByteArrayInputStream(content.getBytes()));
 
         when(s3Client.getObject((GetObjectRequest) any())).thenReturn(mockResponse);
 
         // Act
-        try (InputStream result = s3Service.getFileInputStream("invoice_20250123.csv")) {
+        try (InputStream result = s3Service.getFileInputStream(INVOICE_NAME)) {
             // Assert
             assertNotNull(result);
-            assertEquals(testContent, new String(result.readAllBytes()));
+            assertEquals(content, new String(result.readAllBytes()));
         }
     }
 
@@ -53,6 +57,6 @@ class S3ServiceTest {
     void getFileInputStream_ShouldWrapS3ExceptionAsIOException() {
         when(s3Client.getObject((GetObjectRequest) any())).thenThrow(S3Exception.builder().message("Error").build());
 
-        assertThrows(IOException.class, () -> s3Service.getFileInputStream("invoice_20250123.csv"));
+        assertThrows(IOException.class, () -> s3Service.getFileInputStream(INVOICE_NAME));
     }
 }
