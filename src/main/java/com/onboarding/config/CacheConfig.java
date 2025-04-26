2530d7a88340381e8bea672ee1c58da02d63d085
ahmedrayf <ahmedrayf@hotmail.com>
2025-04-27 02:10:02 +0300

    Enhance caching with time to live.

--- src/main/java/com/onboarding/config/CacheConfig.java
+++ src/main/java/com/onboarding/config/CacheConfig.java
@@ -1,17 +1,31 @@
 package com.onboarding.config;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
 
 @Configuration
 @EnableCaching
 public class CacheConfig {
 
+    @Value("${cache.TTL}")
+    private int duration;
+    @Value("${cache.max-size}")
+    private int maxSize;
     @Bean
     public CacheManager cacheManager() {
-        return new ConcurrentMapCacheManager("accountInvoices");
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager("accountInvoices");
+        cacheManager.setCaffeine(
+                Caffeine.newBuilder()
+                        .expireAfterWrite(Duration.ofMinutes(duration))
+                        .maximumSize(maxSize)
+        );
+        return cacheManager;
     }
 }
--- src/test/java/com/onboarding/config/CacheTestConfig.java
+++ src/test/java/com/onboarding/config/CacheTestConfig.java
@@ -1,11 +1,15 @@
 package com.onboarding.config;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.time.Duration;
 
 
 @TestConfiguration
@@ -13,9 +17,18 @@ import org.springframework.test.context.ActiveProfiles;
 @EnableCaching
 public class CacheTestConfig {
 
-
+    @Value("${cache.TTL}")
+    private int duration;
+    @Value("${cache.max-size}")
+    private int maxSize;
     @Bean
     public CacheManager cacheManager() {
-        return new ConcurrentMapCacheManager("accountInvoices");
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager("accountInvoices");
+        cacheManager.setCaffeine(
+                Caffeine.newBuilder()
+                        .expireAfterWrite(Duration.ofMinutes(duration))
+                        .maximumSize(maxSize)
+        );
+        return cacheManager;
     }
 }
