dba8fa06aa37e0ef0eab91d8164feae7dcb1c593
ahmedrayf <ahmedrayf@hotmail.com>
2025-04-28 12:08:32 +0300

    Move Async config properties to yml .

--- src/main/java/com/onboarding/config/AsyncConfig.java
+++ src/main/java/com/onboarding/config/AsyncConfig.java
@@ -1,6 +1,7 @@
 package com.onboarding.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -11,13 +12,23 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 @Slf4j
 public class AsyncConfig {
 
+    @Value("${async.invoice.name}")
+    private String invoiceAsyncName;
+    @Value("${async.invoice.core-pool-size}")
+    private int corePoolSize;
+    @Value("${async.invoice.max-pool-size}")
+    private int maxPoolSize;
+    @Value("${async.invoice.queue-capacity}")
+    private int queueCapacity;
+
+
     @Bean
     public ThreadPoolTaskExecutor taskExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(2);
-        executor.setMaxPoolSize(2);
-        executor.setQueueCapacity(100);
-        executor.setThreadNamePrefix("InvoiceAsync");
+        executor.setCorePoolSize(corePoolSize);
+        executor.setMaxPoolSize(maxPoolSize);
+        executor.setQueueCapacity(queueCapacity);
+        executor.setThreadNamePrefix(invoiceAsyncName);
         executor.setRejectedExecutionHandler((r, executor1) -> log.warn("Task rejected, thread pool is full and queue is also full"));
         executor.initialize();
         return executor;
